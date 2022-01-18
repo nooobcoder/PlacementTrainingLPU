cae0671381d9b1adf04b84c6f875739cd118d92f
nooobcoder <suryashi2013@gmail.com>
2022-01-18 19:36:11 +0530

    Add TrappingRainWater.java

---
+++ day0/TrappingRainWater.java
@@ -0,0 +1,26 @@
+package day0;
+
+import java.util.Stack;
+
+public class TrappingRainWater {
+    public int trap(int[] A) {
+        int cur = 0;
+        while (cur < A.length && A[cur] == 0)
+            ++cur;
+
+        int vol = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        while (cur < A.length) {
+            while (!stack.isEmpty() && A[cur] >= A[stack.peek()]) {
+                int b = stack.pop();
+                if (stack.isEmpty())
+                    break;
+                vol += ((cur - stack.peek() - 1) * (Math.min(A[cur], A[stack.peek()]) - A[b]));
+            }
+            stack.push(cur);
+            ++cur;
+        }
+
+        return vol;
+    }
+}
