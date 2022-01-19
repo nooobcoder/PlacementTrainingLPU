4cd35e48be9d5d6b8d6cfd782f916ea13b5e8e9f
nooobcoder <suryashi2013@gmail.com>
2022-01-19 18:24:03 +0530

    Add SortArray.cpp

---
+++ C++/day1/SortArray.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// Source: https://leetcode.com/problems/sort-colors/
+
+void sort012(vector<int> &nums) {
+    int p0{0}, p1{0}, p2 = nums.size() - 1;
+
+    while (p1 <= p2) {
+        switch (nums[p1]) {
+            case 0:
+                swap(nums[p0], nums[p1]);
+                p0++;
+                p1++;
+                break;
+            case 1:
+                p1++;
+                break;
+            case 2:
+                swap(nums[p1], nums[p2]);
+                p2--;
+                break;
+        }
+    }
+}
+
+int main() {
+
+}
