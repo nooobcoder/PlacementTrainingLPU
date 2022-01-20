999379675e4a9db278418a3a4e8569d42a26ece2
nooobcoder <suryashi2013@gmail.com>
2022-01-20 20:34:24 +0530

    Day 1 Tutorial problems

---
+++ JAVA/day1/TrappingRainwater.java
@@ -0,0 +1,9 @@
+package JAVA.day1;
+
+// Source: https://leetcode.com/problems/trapping-rain-water/
+
+public class TrappingRainwater {
+    public static void main(String[] args) {
+        System.out.println("Nice");
+    }
+}
